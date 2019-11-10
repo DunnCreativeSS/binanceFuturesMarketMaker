d3b79a0d476b265a06d2465bae79df9d3cbc7af3
Your Name <you@example.com>
2019-11-10 17:33:45 +0000

    configs

--- configs/create.py
+++ configs/create.py
@@ -1,25 +1,20 @@
 import json
-a = 1.25
+a = 0.75
 b = 1.25
 c = 75
-d = 1.25
-while a <= 2.5:
+while a <= 1.5:
 	while b <= 2.5:
 		while c <= 150:
-			while d <= 2.5:
-				json2 = {
+			json2 = {
 "RISK_CHARGE_VOL": {"current": a, "min": 1, "max": 4, "step": 1},
 "EWMA_WGT_COV": {"current": b, "min": 1, "max": 4, "step": 1},
-"VOL_PRIOR":  {"current": c, "min": 50, "max": 300, "step": 50},
-"DECAY_POS_LIM":  {"current": d, "min": 1, "max": 4, "step": 1}
+"VOL_PRIOR":  {"current": c, "min": 50, "max": 300, "step": 50}
 }
-				with open('config-' + str(a) + '-' + str(b) + '-' + str(c) + '-' + str(d) + '.json', 'w') as outfile:
-   					json.dump(json2, outfile)
-				d = d * 2
-				print(a)
-			d = 1
+			with open('config-' + str(a) + '-' + str(b) + '-' + str(c) + '.json', 'w') as outfile:
+					json.dump(json2, outfile)
+			
 			c = c * 2
-		c = 50
+		c = 75
 		b = b * 2
-	b = 1
+	b = 1.25
 	a = a * 2
